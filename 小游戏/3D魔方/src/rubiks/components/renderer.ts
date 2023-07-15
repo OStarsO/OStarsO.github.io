1216e7b64ca444eeaecae85ddba443abfe4aa07c
OStarsO <131366530+OStarsO@users.noreply.github.com>
2023-07-15 09:41:23 +0800

    Delete 小游戏/3D魔方 directory

--- 小游戏/3D魔方/src/rubiks/components/renderer.ts
+++
@@ -1,9 +0,0 @@
-import {WebGLRenderer} from "three";
-
-const createRenderer = () => {
-    const renderer = new WebGLRenderer({antialias: true});
-    
-    return renderer;
-};
-
-export default createRenderer;
