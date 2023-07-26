df4b9df94221232bf2180c2137ca3645bf3cad73
OStarsO <131366530+OStarsO@users.noreply.github.com>
2023-07-26 16:52:34 +0800

    Add files via upload

---
+++ 小游戏/地铁跑酷/_nuxt/c1742934.js
@@ -0,0 +1,1 @@
+import{B as e}from"./entry.6a3889e5.js";function r(){return e().$device}export{r as u};
---
+++ 小游戏/地铁跑酷/_nuxt/dd0ae419.js
@@ -0,0 +1,1 @@
+import{J as s}from"./entry.6a3889e5.js";const a=()=>s("historyCounter",()=>0),e=()=>s("favoriteCounter",()=>0),o=()=>s("seCatch",()=>!1),r=()=>s("gameStart",()=>!1),u=()=>s("loading",()=>!1),n=()=>s("sort",()=>"default"),c=()=>s("adblock",()=>!1);export{o as a,a as b,e as c,n as d,r as e,c as f,u};
