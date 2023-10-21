801bddd27bb92f40ff3f639ab78a5a35d55881d8
OStarsO <131366530+OStarsO@users.noreply.github.com>
2023-10-21 09:55:48 +0800

    script.js

--- 音乐播放器/script.js
+++ 音乐播放器/script.js
@@ -21,7 +21,7 @@ new Vue({
           artist: "Norm Ender",
           cover: "https://github.com/OStarsO/OStarsO.github.io/blob/62a66585b17873966d87f31893bc14e1a7c189f8/%E9%9F%B3%E4%B9%90/Mine/%E5%9B%BE%E7%89%87.jpg",
           source: "https://github.com/OStarsO/OStarsO.github.io/blob/445d6201efd3b42d4d46863b60a89b7b95a5000f/%E9%9F%B3%E4%B9%90/Mine/Mine.m4a",
-          url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+          url: "https://music.163.com/#/song?id=30780496",
           favorited: false
         },
         {
